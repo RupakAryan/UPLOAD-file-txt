2546569e851ddbfeeda96e5bb5e61e66e6cc9b6c
RupakAryan <157547044+RupakAryan@users.noreply.github.com>
2026-02-08 16:45:19 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,19 +1,19 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+ab#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "29057526"))
-API_HASH = environ.get("API_HASH", "92cb1f82af717e97a2ad7e1670c35b21")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8241696754:AAFK_xH_EqkIY7C2nfoY1W44XzDbCSRlIK4")
+API_ID = int(environ.get("API_ID", "29978901"))
+API_HASH = environ.get("API_HASH", "500fc876c5356cf04ed3698912dc2edf")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8464714544:AAEhz5jgFxhf3_mHy1oHOTynWBDCfSzkIAU")
 
-OWNER = int(environ.get("OWNER", "7338678521"))
+OWNER = int(environ.get("OWNER", "5776977809"))
 CREDIT = environ.get("CREDIT", "blackheart.ex")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '7338678521').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '5776977809').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
